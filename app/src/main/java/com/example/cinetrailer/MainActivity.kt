863c08adcdaf3f1e2945051ad897b3c26203acc7
Julio Morais <eujuliomateusmorais@gmail.com>
2026-01-10 19:31:39 -0300

    feat: implementa serviço de notificação

--- app/src/main/java/com/example/cinetrailer/MainActivity.kt
+++ app/src/main/java/com/example/cinetrailer/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.cinetrailer
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,18 +10,37 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.cinetrailer.ui.components.BottomNavigationBar
 import com.example.cinetrailer.ui.components.CineTopBar
 import com.example.cinetrailer.ui.navigation.AppNavigation
 import com.example.cinetrailer.ui.theme.CineTrailerTheme
-
+import com.example.cinetrailer.worker.MovieNotificationWorker
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import java.util.concurrent.TimeUnit
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        scheduleMovieNotification()
         setContent {
             CineTrailerTheme {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    val permissionState = rememberPermissionState(Manifest.permission.POST_NOTIFICATIONS)
+
+                    LaunchedEffect(Unit) {
+                        if (!permissionState.status.isGranted) {
+                            permissionState.launchPermissionRequest()
+                        }
+                    }
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -28,6 +49,19 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+    private fun scheduleMovieNotification() {
+        val workRequest = PeriodicWorkRequestBuilder<MovieNotificationWorker>(
+            15, TimeUnit.MINUTES
+        )
+            .setInitialDelay(5, TimeUnit.SECONDS)
+            .build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "DailyMovieSuggestion",
+            ExistingPeriodicWorkPolicy.UPDATE,
+            workRequest
+        )
     }
 }
 
