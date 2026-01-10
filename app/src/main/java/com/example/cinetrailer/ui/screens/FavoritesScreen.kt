40c2e8aed74b20a99cb611886b7562fa33483378
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2026-01-10 08:01:52 -0300

    feat: building the favorite screen

--- app/src/main/java/com/example/cinetrailer/ui/screens/FavoritesScreen.kt
+++ app/src/main/java/com/example/cinetrailer/ui/screens/FavoritesScreen.kt
@@ -1,9 +1,30 @@
 package com.example.cinetrailer.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FavoritesScreen() {
-    Text("Favoritos", color = androidx.compose.ui.graphics.Color.White)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF121212))
+    ) {
+        Text(
+            text = "Meus Favoritos",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
